4f3bacef04e80ea33dadeda9b47e8f9ea56c1e75
Souradeep Das <souradeep.das1995@gmail.com>
2022-04-26 17:54:02 +0530

    added props to ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,18 @@
 import AddToCardIcon from "../assets/icons/add_cart.svg";
 
-const ListItem = () => {
+const ListItem = (props) => {
     return (
         <div className="item-card">
             <img className="img-fluid" src="assets/placeholder.png" alt="Placeholder" />
             <div className="item-card__information">
                 <div className="pricing">
-                    <span>Discounted Price</span>
+                    <span>{props.data.discountedPrice}</span>
                     <small>
-                        <strike>Actual Price</strike>
+                        <strike>{props.data.actualPrice}</strike>
                     </small>
                 </div>
                 <div className="title">
-                    <h3>Item Name</h3>
+                    <h3>{props.data.itemName}</h3>
                 </div>
             </div>
             <button className="cart-add">
