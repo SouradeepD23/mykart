04b3bd119b7205ed112c34d11ec5244ed9da242c
Souradeep Das <souradeep.das1995@gmail.com>
2022-05-02 13:15:06 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Products from "./components/Products";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Products></Products>
     </div>
   );
